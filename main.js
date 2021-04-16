677674255361022fc05aca35b9eccfdca7d2f862
pantchayan <pantchayan@gmail.com>
2021-04-17 02:19:00 +0530

    Implemented Uber Fetch

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-let form = document.querySelector("form");
+let puppeteer = require("puppeteer");
+
+let uberFetch = require("./cabs/uber");
+let olaFetch = require("./cabs/ola");
+let meruFetch = require("./cabs/meru");
+
+
+// let form = document.querySelector("form");
 
 // input selectors
-let srcSelectors = document.querySelector("#source");
-let destSelectors = document.querySelector("#destination");
-let citySelectors = document.querySelector("#cities");
+// let srcSelectors = document.querySelector("#source");
+// let destSelectors = document.querySelector("#destination");
+// let citySelectors = document.querySelector("#cities");
 
-form.addEventListener("submit", (e) => {
-       e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//        e.preventDefault();
        
-       if(!srcSelectors.value){
-              alert("Please enter source")
-       }
-       else if(!destSelectors.value){
-              alert("Please enter destination");
-       }
-       else{
-              getCabDetails(citySelectors.value, srcSelectors.value, destSelectors.value);
-       }
-});
+//        if(!srcSelectors.value){
+//               alert("Please enter source")
+//        }
+//        else if(!destSelectors.value){
+//               alert("Please enter destination");
+//        }
+//        else{
+//               getCabDetails(citySelectors.value, srcSelectors.value, destSelectors.value);
+//        }
+// });
+
 
 let getCabDetails = async (city, source, dest) => {
        console.log(source,dest,city);
@@ -27,12 +35,15 @@ let getCabDetails = async (city, source, dest) => {
               args: ["--start-maximized"],
        });
 
-       
-       // let uberArr = await getUber(city, source, dest);
-       // let olaArr = await getOla(city, source, dest);
-       // let meruArr = await getMeru(city, source, dest);
 
-       // console.table(uberArr);
+       let uberArr = await uberFetch.getUber(city, source, dest, browserInstance);
+       // let olaArr = await getOla(city, source, dest, browserInstance);
+       // let meruArr = await getMeru(city, source, dest, browserInstance);
+
+       console.table(uberArr);
        // console.table(olaArr);
        // console.table(meruArr);
 }
+
+
+getCabDetails("Delhi", "Greater Noida", "Ghaziabad");
